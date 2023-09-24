e149064feb63d80df97aef59b4f5436ac5824311
peesock <kcormn@gmail.com>
2023-09-24 16:04:55 -0700

    change completion scheme

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -40,7 +40,7 @@ return {
 					documentation = cmp.config.window.bordered(),
 				},
 
-				mapping = cmp.mapping.preset.insert({
+				--[[ mapping = cmp.mapping.preset.insert({
 					--- tab completion
 					["<Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
@@ -64,61 +64,84 @@ return {
 						end
 					end, { "i", "s" }),
 					["<CR>"] = cmp.mapping.confirm({ select = false }),
+				}), ]]
 
-						--- not tab completion
-						-- ["<C-p>"] = cmp.select_prev_item(),
-						-- ["<C-n>"] = cmp.select_next_item(),
-						-- ["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-						["<C-b>"] = cmp.mapping.scroll_docs(-4),
-						["<C-f>"] = cmp.mapping.scroll_docs(4),
-						["<C-Space>"] = cmp.mapping.complete(),
-						-- ["<S-Tab>"] = cmp.mapping.abort(),
-					}),
-					sources = cmp.config.sources({
-						{ name = "nvim_lsp", max_item_count = 10 },
-						-- { name = "vsnip" }, -- For vsnip users.
-						{ name = 'luasnip' }, -- For luasnip users.
-						-- { name = 'ultisnips' }, -- For ultisnips users.
-						-- { name = "snippy", max_item_count = 10 }, -- For snippy users.
-						{ name = "buffer", max_item_count = 10 },
-						{ name = "path", max_item_count = 10 },
-					}),
-				})
+				mapping = {
+					["<C-p>"] = cmp.mapping.select_prev_item(),
+					["<C-n>"] = cmp.mapping.select_next_item(),
+					["<C-d>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.close(),
+					["<CR>"] = cmp.mapping.confirm {
+						behavior = cmp.ConfirmBehavior.Insert,
+						select = false,
+					},
+					["<Tab>"] = cmp.mapping(function(fallback)
+						-- if cmp.visible() then
+						-- 	cmp.select_next_item()
+						if luasnip.expand_or_jumpable() then
+							vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+						else
+							fallback()
+						end
+					end, { "i", "s", }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						-- if cmp.visible() then
+						-- 	cmp.select_prev_item()
+						if luasnip.jumpable(-1) then
+							vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+						else
+							fallback()
+						end
+					end, { "i", "s", }),
+				},
 
-				-- Set configuration for specific filetype.
-				cmp.setup.filetype("gitcommit", {
-					sources = cmp.config.sources({
-						{ name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
-					}, {
-						{ name = "buffer" },
-					}),
-				})
+				sources = {
+					{ name = "nvim_lsp", max_item_count = 10 },
+					-- { name = "vsnip" }, -- For vsnip users.
+					{ name = 'luasnip' }, -- For luasnip users.
+					-- { name = 'ultisnips' }, -- For ultisnips users.
+					-- { name = "snippy", max_item_count = 10 }, -- For snippy users.
+					{ name = "buffer", max_item_count = 10 },
+					{ name = "path", max_item_count = 10 },
+				},
+			})
 
-				-- -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-				-- cmp.setup.cmdline({ "/", "?" }, {
-				-- 	mapping = cmp.mapping.preset.cmdline(),
-				-- 	sources = {
-				-- 		{ name = "buffer" },
-				-- 	},
-				-- })
-				--
-				-- -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-				-- cmp.setup.cmdline(":", {
-				-- 	mapping = cmp.mapping.preset.cmdline(),
-				-- 	sources = cmp.config.sources({
-				-- 		{ name = "path" },
-				-- 	}, {
-				-- 		{ name = "cmdline" },
-				-- 	}),
-				-- })
-			end,
-		},
+			-- Set configuration for specific filetype.
+			cmp.setup.filetype("gitcommit", {
+				sources = cmp.config.sources({
+					{ name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
+				}, {
+					{ name = "buffer" },
+				}),
+			})
 
-		{
-			'L3MON4D3/LuaSnip',
-			build = 'make install_jsregexp',
-			config = function()
-				-- require('luasnip').config({
+			--[[ -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+
+			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+			}) ]]
+		end,
+	},
+
+	{
+		'L3MON4D3/LuaSnip',
+		build = 'make install_jsregexp',
+		config = function()
+			-- require('luasnip').config({
 				--
 				-- })
 			end
