341e28f77cf9cf94a682ea859232a289f721f5c3
peesock <kcormn@gmail.com>
2023-09-24 16:03:23 -0700

    change diagnostic underlining

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -2,8 +2,23 @@ function ColorMyPencils(color)
 	color = color or "onedark_vivid"
 	vim.cmd.colorscheme(color)
 
+	-- some colorizor mods
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "LineNr", { guibg = "none"})
+	vim.api.nvim_set_hl(0, "FloatBorder", { fg = "white" })
+	vim.api.nvim_set_hl(0, "DiagnosticInfo", { sp = "none" })
+
+	-- remove text highlighting but not underlighting of diagnostics
+	for _, value in pairs({"Ok", "Hint", "Info", "Warn", "Error",}) do
+		vim.api.nvim_set_hl(0, "DiagnosticUnderline" .. value, {
+			sp = vim.api.nvim_get_hl(0, { name = "DiagnosticUnderline" .. value }).fg,
+			underline = true,
+			-- undercurl = true,
+			-- underdouble = true;
+			-- underdashed = true,
+			-- underdotted = true,
+		})
+	end
+	-- vim.api.nvim_set_hl(0, "LineNr", { bg = "none"})
 end
-ColorMyPencils("onedark_vivid")
+ColorMyPencils("onedark_vivid") -- initialize with my most beloved theme
