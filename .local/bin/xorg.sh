d89469f4c1879cd4331264f7a91b18698d18c07c
peesock <kcormn@gmail.com>
2023-10-06 13:53:16 -0700

    fix xss-lock buggery

--- .local/bin/xorg.sh
+++ .local/bin/xorg.sh
@@ -10,7 +10,7 @@ xset r rate 300 50 # keyboard 300ms delay and 50hz repeat rate
 
 xset s on
 xset s noblank # prevents monitor from shutting off (nvidia + monitor bug of mine), displays pattern instead (unless screen saver/locker is set)
-xset -dpms # disable dpms; i want to configure everything in my Xorg config
+xset -dpms # disable dpms; i shrimply don't want it
 
-xset s 900 10 # set screen saver/locker timeout
-xss-lock -n 'apock warn' -- apock &
+xset s 900 15 # set screen saver/locker timeout
+setsid -f xss-lock -n 'apock warn' -- apock
