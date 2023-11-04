e35d8f87deaefdb140bb16c5ce6e7fae8040dcb7
peesock <kcormn@gmail.com>
2023-11-03 19:03:26 -0700

    better browser sync

--- .local/bin/firefox.sh
+++ .local/bin/firefox.sh
@@ -1,6 +1,5 @@
-#!/bin/sh
-browser-sync firefox ~/.mozilla/firefox >/dev/null
-firefox-developer-edition & ffpid=$!
-browser-sync -d -e firefox ~/.mozilla/firefox >/dev/null &
-trap 'kill -s INT $ffpid' INT TERM
-wait
+#!/bin/sh -m
+browser-sync firefox ~/.mozilla/firefox
+firefox-developer-edition &
+browser-sync -d -e firefox ~/.mozilla/firefox &
+fg %-
