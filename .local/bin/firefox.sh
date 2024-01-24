c8cacfc1558fd7d0b291eee4e62d027625ad1b24
peesock <kcormn@gmail.com>
2024-01-24 00:50:28 -0700

    now works with ff-dev-edition, no more tty issues

--- .local/bin/firefox.sh
+++ .local/bin/firefox.sh
@@ -1,5 +1,5 @@
-#!/bin/sh -m
-browser-sync firefox ~/.mozilla/firefox
+#!/bin/sh
+browser-sync firefox-developer-edition firefox ~/.mozilla/firefox
 firefox-developer-edition &
-browser-sync -d -e firefox ~/.mozilla/firefox &
-fg %-
+browser-sync -d -e firefox-developer-edition firefox ~/.mozilla/firefox &
+wait
