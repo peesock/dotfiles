624d527bb53ef86170d420a8553e88f8549a00c0
peesock <kcormn@gmail.com>
2024-02-21 13:34:24 -0700

    huge memory-sync change

--- .local/bin/firefox.sh
+++ .local/bin/firefox.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 memory-sync firefox ~/.mozilla/firefox
 firefox-developer-edition &
-memory-sync -d -e firefox ~/.mozilla/firefox -p $!
+memory-sync -D -e firefox ~/.mozilla/firefox -p $!
