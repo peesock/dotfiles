c96a6ddb1dda6fb23c8e30c4f5e8418ae343b232
peesock <kcormn@gmail.com>
2024-02-21 22:28:08 -0700

    memory-sync improvements, runit service

--- .local/bin/firefox.sh
+++ .local/bin/firefox.sh
@@ -1,4 +1,35 @@
 #!/bin/sh
-memory-sync firefox ~/.mozilla/firefox
+
+dir=~/.mozilla/firefox/
+sync(){
+	memory-sync -c firefox $dir ||
+		memory-sync firefox $dir
+}
+checksync(){
+	echo checking sync
+	[ "$1" ] && {
+		sleep "$1" & # must be >1
+	}
+	svu check firefox-ram | LC_ALL=C grep '^ok: run: .\+) 1s' || {
+		[ -d /proc/$! ] || return 1
+		sleep 0.1
+		checksync
+	}
+	return 0
+}
+
+sync
+svu check firefox-ram >/dev/null && {
+	if svu check firefox-ram | LC_ALL=C grep '^ok: run: ' >/dev/null; then
+		exec firefox-developer-edition
+	else
+	svu u firefox-ram
+	checksync 3 &&
+		exec firefox-developer-edition
+	fi
+}
+echo 'service not working, using local daemon'
+trap 'kill $!' INT TERM
 firefox-developer-edition &
-memory-sync -D -e firefox ~/.mozilla/firefox -p $!
+memory-sync -D -e -u 30m -t 5 firefox "$dir" "/usr/lib/firefox-developer-edition/firefox"
+wait $!
--- .zlogout
+++ .zlogout
@@ -1,9 +1,3 @@
-i=$(who | grep -F "$USER" | wc -l)
+# i=$(who | grep -F "$USER" | wc -l)
 # last tty logout but not really due to ssh issue that i am not addressing
-if [ $i -eq 1 ] && who | grep "^$USER\s*tty" >/dev/null; then
-	mpc pause >/dev/null 2>&1
-	[ -d "$(readlink "$HOME/.mozilla/firefox")" ] &&
-		browser-sync firefox ~/.mozilla/firefox
-
-	# pkill -u "$USER"
-fi
+# :appel:
