e2a7600752e535f8de1ee18046ca58c5f8eb87e6
peesock <kcormn@gmail.com>
2024-02-28 23:36:22 -0700

    memsync bug fixes, improvements

--- .local/bin/firefox.sh
+++ .local/bin/firefox.sh
@@ -6,7 +6,7 @@ checksync(){
 	[ "$1" ] && {
 		sleep "$1" & # must be >1
 	}
-	lastChar=$(svu check firefox-ram | LC_ALL=C grep -o '^ok: run: .\+) [0-9]' | tail -c2) && [ "$lastChar" -gt 0 ] || {
+	lastChar=$(svu check firefox-ram | LC_ALL=C grep -o '^ok: run: .\+) [0-9]' | tail -c2) && [ "$lastChar" -gt 0 ] 2>/dev/null || {
 		[ -d /proc/$! ] || return 1
 		sleep 0.1
 		checksync
@@ -14,18 +14,21 @@ checksync(){
 	return 0
 }
 
-memory-sync -c firefox "$dir"
+memory-sync firefox "$dir" 2>/dev/null & pid=$!
 svu check firefox-ram >/dev/null && {
 	if svu check firefox-ram | LC_ALL=C grep '^ok: run: ' >/dev/null; then
 		exec firefox-developer-edition
 	else
 	svu u firefox-ram
-	checksync 3 &&
+	checksync 5 && {
+		wait $pid
 		exec firefox-developer-edition
+	}
 	fi
 }
 echo 'service not working, using local daemon'
 trap 'kill $!' INT TERM
+wait $pid
 firefox-developer-edition &
 memory-sync -D -e -u 30m -t 5 firefox "$dir" "firefox"
 wait $!
