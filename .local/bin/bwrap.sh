5300aa88853ff5cc744c8a19abb1f218e6a66f34
peesock <kcormn@gmail.com>
2024-03-25 02:55:58 -0700

    bwrap update

--- .local/bin/bwrap.sh
+++ .local/bin/bwrap.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+set -e
 # bwrap boilerplate. not meant to work everywhere, only work well enough to quickly sandbox my personal stuff.
+XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR-"/run/user/$(id -u)"}
+WINEPREFIX=${WINEPREFIX-"$HOME/.wine"}
 executer(){
 	arg=$(eval "$2" | while read -r file; do
 		echo "$1 $file $file"
@@ -16,16 +19,21 @@ while true; do
 			args="$args --unshare-all --share-net"
 			shift
 			continue;;
+		-env)
+			arg=$(for var in $2; do eval 'printf -- "--setenv %s %s\n" "$var" "$'"$var"'"' ; done)
+			args="$args --clearenv $arg"
+			shift 2
+			continue;;
 		-root)
 			args="$args --unshare-user --uid 0 --gid 0 --setenv USER root --setenv HOME /root"
 			shift
 			continue;;
 		-wine)
-			args="$args --bind-try ${WINEPREFIX-$HOME/.wine} ${WINEPREFIX-$HOME/.wine}"
+			args="$args --bind-try $WINEPREFIX $WINEPREFIX"
 			shift
 			continue;;
 		-xorg)
-			args="$args --bind-try /tmp/.X11-unix /tmp/.X11-unix"
+			args="$args --ro-bind-try /tmp/.X11-unix /tmp/.X11-unix"
 			shift
 			continue;;
 		-exec)
@@ -36,9 +44,24 @@ while true; do
 			executer --dev-bind-try 'find /dev -maxdepth 1 -name nvidia\*'
 			shift
 			continue;;
-		-audio)
-			executer --bind 'find /run/user/$(id -u) -maxdepth 1 | grep "/pipewire\|/pulse" ; echo /dev/snd'
+		-cpu)
+			args="$args --dev-bind-try /sys/devices/system/cpu /sys/devices/system/cpu"
 			shift
+			continue;;
+		-audio)
+			executer --ro-bind 'find "$XDG_RUNTIME_DIR" -maxdepth 1 | grep "/pipewire\|/pulse"'
+			shift
+			continue;;
+		-preset)
+			case $2 in
+				game)
+					arg='-more -wine -xorg -nvidia -cpu -audio'
+					;;
+				*)
+					exit 1;;
+			esac
+			shift 2
+			set -- $arg "$@"
 			continue;;
 	esac
 	break
@@ -48,7 +71,7 @@ bwrap \
 --ro-bind /usr/share /usr/share/ \
 --ro-bind /usr/lib /usr/lib \
 --ro-bind /usr/lib32 /usr/lib32 \
---symlink /usr/lib /usr/lib64 \
+--symlink lib /usr/lib64 \
 --symlink /usr/lib /lib64 \
 --symlink /usr/lib /lib \
 --symlink /usr/bin /bin \
@@ -57,5 +80,6 @@ bwrap \
 --tmpfs /run \
 --proc /proc \
 --dev /dev \
+--die-with-parent \
 $args \
 "$@"
