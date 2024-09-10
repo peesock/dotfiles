757cd5b3056436624beb5a046ef5c5377893c7c7
peesock <kcormn@gmail.com>
2024-09-10 14:45:31 -0700

    overlay.sh:   - remove tmpfser   - only overlay mount is in tmpfs   - better handling, purer functions   - foldin/out allows all files   - add automount

--- .local/bin/overlay.sh
+++ .local/bin/overlay.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-set -x
+# set -x
 # rules:
 # images in use should be in the root directory
 # lower stores multiple mountpoints
 
 log(){
-	echo "${0##*/}:" "$@"
+	printf '%s\n' "${0##*/}: $*"
 }
 
 escapist(){ # to store arrays as escaped single quoted arguments
@@ -14,13 +14,18 @@ escapist(){ # to store arrays as escaped single quoted arguments
 }
 
 foldout(){ # /path/to/dir -> /path/to/dir/dir
-	for arg; do
-		tmp=$(mktemp -dp "${arg%/*}")
-		mv -Tf "$arg" "$tmp"
+	until [ $# -eq 0 ]; do
+		arg=$1
+		pickup=$2
+		tmp=$(mktemp -up "${arg%/*}")
+		mv -T "$arg" "$tmp" || return 1
 		mkdir "$arg"
 		mv -T "$tmp" "$arg/${arg##*/}"
-		foldinlist="$foldinlist $(escapist "$arg") $(stat -c %a "$arg")"
-		chmod a-w "$arg"
+		[ "$pickup" = 1 ] && {
+			foldinlist="$foldinlist $(escapist "$arg")$(stat -c %a "$arg")"
+			chmod a-w "$arg"
+		}
+		shift 2
 		log moved "'$arg'" to "'$arg/${arg##*/}'"
 	done
 }
@@ -30,9 +35,10 @@ foldin(){ # /path/to/dir/dir -> /path/to/dir
 		arg=$1
 		octal=$2
 		chmod "$octal" "$arg"
-		tmp=$(mktemp -dp "${arg%/*}")
-		mv -Tf "$arg/${arg##*/}" "$tmp"
-		mv -Tf "$tmp" "$arg" || { mv -T "$tmp" "$arg/${arg##*/}"; return 1; }
+		tmp=$(mktemp -up "${arg%/*}")
+		mv -T "$arg/${arg##*/}" "$tmp"
+		rmdir "$arg" || { mv -T "$tmp" "$arg/${arg##*/}"; return 1; }
+		mv -T "$tmp" "$arg"
 		shift 2
 		log moved "'$arg/${arg##*/}'" to "'$arg'"
 	done
@@ -40,82 +46,43 @@ foldin(){ # /path/to/dir/dir -> /path/to/dir
 
 loweradd(){
 	for arg; do
+		case $arg in *:*) log "'$arg'" contains "':'", which breaks fuse-overlayfs.; exit 1;; esac
 		if [ -d "$arg/${arg##*/}" ] && [ "$(find "$arg" -maxdepth 1 -print0 | head -zn3 | tr -cd '\0' | wc -c)" -eq 2 ]; then
 			log "'$arg'" already in correct format
 		else
-			foldoutlist="$foldoutlist $(escapist "$arg")"
+			foldoutlist="$foldoutlist $(escapist "$arg")1"
 		fi
 		# escaping colons SHOULD work, but it doesn't, because it's not yet implemented
 		lowerdirs=$lowerdirs:$(printf %s "$arg" | sed 's/\([,:\\]\)/\\\1/g')
-		# if [ -n "$lowerdirs" ]; then
-		# 	lowerdirs=$lowerdirs:$(printf %s "$arg" | sed 's/\([,:\\]\)/\\\1/g')
-		# else
-		# 	lowerdirs=$(printf %s "$arg" | sed 's/\([,:\\]\)/\\\1/g')
-		# fi
 	done
 }
 lowerdirs=lower
 
-tmpfser(){
-	upperdir=$tmppath/upper
-	workdir=$tmppath/work
-	if [ "$1" = "unmount" ]; then
-		mv -T "$workdir" "$path/work"
-		# mv -T "$upperdir" "$path/upper"
-		rsync -aHAWXS --numeric-ids --delete "$upperdir/" "$path/upper"
-		rm -rf "$upperdir"
-	else # mount
-		mv -T "$path/work" "$workdir"
-		# mv -T "$path/upper" "$upperdir"
-		mkdir "$upperdir"
-		rsync -aHAWXS --numeric-ids "$path/upper/" "$upperdir"
-	fi
-}
-
 creator(){
 	if [ -e "$path" ]; then
 		log "'$path'" exists, moving into folder of same name
-		tmp=$(mktemp -up "${path%/*}")
-		mv "$path" "$tmp"
-		mkdir "$path"
-		mv "$tmp" "$path/${path##*/}"
+		foldout "$path" 0
 	else
 		mkdir "$path"
 	fi
 	cd "$path" || exit
-	mkdir upper work
+	mkdir lower upper work
 	log created template
 }
 
 mounter(){
-	cd "$tmppath" || exit
-	mkdir -p lower overlay || exit
-	[ "$dwarfs" ] && {
-		cd "$path" || exit
-		for dwarf in *.dwarfs; do
-			mkdir -p "$tmppath/lower/${dwarf%.*}"
-			dwarfs "$dwarf" "$tmppath/lower/${dwarf%.*}" && log mounted "'$dwarf'"
-		done
-		cd "$OLDPWD" || exit
-	}
-	[ "$tmpfs" ] && tmpfser
-	[ -n "$wine" ] && {
-		log updating wine...
-		WINEPREFIX="$wine" WINEDEBUG=-all DISPLAY='' WAYLAND_DISPLAY='' wineboot -u
-		export WINEPREFIX="$PWD/overlay/.wine-ro"
-	}
+	cd "$path" || exit
+	for d in lower upper work; do [ -d "$d" ] || exit; done
+	mkdir -p "$overlaydir"
 	[ -n "$foldoutlist" ] && eval foldout "$foldoutlist"
-	fuse-overlayfs -o lowerdir="$lowerdirs" -o upperdir="$upperdir" -o workdir="$workdir" overlay && log mounted overlayfs || {
-		s=$?
-		[ "$tmpfs" ] && tmpfser unmount
-		exit $s
-	}
+	fuse-overlayfs -o lowerdir="$lowerdirs" -o upperdir=upper -o workdir=work "$overlaydir" && log mounted overlayfs || exit
+	ln -sfnT "$overlaydir" "$path"/overlay
 }
 
 umounter(){
-	cd "$tmppath" || exit
+	cd "$path" || exit
 	s=0
-	fusermount3 -u overlay && log unmounted overlayfs || return 1
+	fusermount3 -u "$overlaydir" && log unmounted overlayfs || return 1
 	cd lower || exit
 	for name in * .*; do
 		case $name in .|..) continue;; esac
@@ -181,29 +148,18 @@ shift
 
 while true; do
 	case $1 in
-		-dwarfs)
+		-automount) # get all paths in root and mount to lower
+			automount=true
+			;;
+		-dwarfs) # find *.dwarfs files and mount to lower
 			dwarfs=true
 			;;
-		-wine)
-			case $func in mount|execute)
-				[ -d ~/.wine-ro ] && wine=$HOME/.wine-ro || wine=${WINEPREFIX-"$HOME"/.wine}
-				loweradd "$wine"
-				;;
-			esac
-			;;
 		-lower)
-			case $2 in *:*) log "'$2'" contains "':'", which breaks fuse-overlayfs.; exit 1;; esac
 			loweradd "$2"
 			shift
 			;;
-		# -bindfs)
-		# 	bindadd "$(realpath -e "$2")" || exit
-		# 	shift
-		# 	;;
-		-tmpfs)
-			# store upper in tmpfs.
-			# this shouldn't be used to workaround filesystem quirks
-			tmpfs=true
+		-wine) # update wine and mount to lower
+			wine=true
 			;;
 		--)
 			shift
@@ -216,13 +172,31 @@ done
 
 [ $# -eq 0 ] && exit 1
 path=$(realpath -m "$1")
-case $path in *:*) log "'$path'" contains "':'", which breaks fuse-overlayfs.; exit 1;; esac
-tmppath=$XDG_RUNTIME_DIR/overlay.sh/$(printf %s "$path" | sha1sum | cut -d' ' -f1)
-mkdir -p "$tmppath"
+overlaydir=$XDG_RUNTIME_DIR/overlay.sh/$(printf %s "$path" | sha1sum | cut -d' ' -f1)
 shift
 
-upperdir=$path/upper
-workdir=$path/work
+[ "$automount" ] && {
+	for p in "$path"/*; do
+		case $p in */upper|*/work|*/overlay|*/lower) continue;; esac
+		loweradd "$p"
+	done
+}
+
+[ "$dwarfs" ] && (
+	cd "$path" || exit
+	for dwarf in *.dwarfs; do
+		mkdir -p "lower/${dwarf%.*}"
+		dwarfs "$dwarf" "lower/${dwarf%.*}" && log mounted "'$dwarf'"
+	done
+)
+
+[ "$wine" ] && {
+	[ -d ~/.wine-ro ] && wine=$HOME/.wine-ro || wine=${WINEPREFIX-"$HOME"/.wine}
+	log updating wine...
+	WINEPREFIX="$wine" WINEDEBUG=-all DISPLAY='' WAYLAND_DISPLAY='' wineboot -u
+	export WINEPREFIX="$overlaydir/.wine-ro"
+	loweradd "$wine"
+}
 
 case $func in
 	create)
