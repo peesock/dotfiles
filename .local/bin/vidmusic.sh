b3f5085d3b48eca13e2f537b6883d4f09ed44510
peesock <kcormn@gmail.com>
2024-11-26 13:33:34 -0700

    updates:   - aliasrc: add niceness   - apock: var name collision bugfix, fixed every screensaver   - vidmusic.sh: use breadth-first cover file search   - zprofile: use neovim manpager

--- .local/bin/vidmusic.sh
+++ .local/bin/vidmusic.sh
@@ -4,7 +4,27 @@ tmp="$(mktemp tmp.XXXXXXX.png)"
 cover=$tmp
 input=$(realpath "$1")
 if [ $# -ge 2 ] || ! ffmpeg -i "$input" "$tmp" -y; then
-	cover=$(find "${2:-"${input%/*}"}" -maxdepth 2 -type f -print0 | xargs -0 file -00 --mime-type -- 2>/dev/null | awk 'BEGIN{RS="\0"} {if((NR % 2) == 0) {if($NF ~ /image\//){ORS="\0"; print var; exit}} else var = $0}')
+	cover=$(
+		dir="${2:-"${input%/*}"}"
+		tmp2=$(mktemp)
+		# breadth-first search for files
+		find "$dir" -maxdepth 1 -type d -print0 > "$tmp2"
+		n=1
+		m=$(tr -cd \\0 < "$tmp2" | wc -c)
+		while [ "$n" -le "$m" ]; do
+			dir=$(head -zn$n < "$tmp2"; echo x)
+			dir=${dir%x}
+			n=$((n + 1))
+			for file in "$dir"/*; do
+				[ -d "$file" ] || printf %s\\0 "$file"
+			done
+		done |
+			xargs -0 file -00 --mime-type -- 2>/dev/null |
+			awk 'BEGIN{RS="\0"} {if((NR % 2) == 0) {if($NF ~ /image\//){ORS="\0"; print var; exit}} else var = $0}'
+		echo x
+		rm "$tmp2"
+	)
+	cover=${cover%x}
 	if [ -z "$cover" ]; then
 		cover=$HOME/download/assets/speaker-orange.png
 	else
