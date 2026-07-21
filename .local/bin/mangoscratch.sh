ccf113491c65c27550dd7009d305c01bd1dc2a99
peesock <kcormn@gmail.com>
2026-07-21 01:23:44 -0700

    mangoscratch add daemon

--- .local/bin/mangoscratch.sh
+++ .local/bin/mangoscratch.sh
@@ -4,31 +4,23 @@
 # todo:
 # more states than float and fullscreen
 # make present() clear other intruding states
-# die and clear with processes
 
-scale=0.5
-while true; do
-	case $1 in
-		-wh) wh=$2; shift 2;;
-		-s) scale=$2; shift 2;;
-		-f) fullscreen=1; shift;;
-		-k) keep=1; shift;;
-		--) shift; break;;
-		*) break;;
-	esac
-done
-idKey=$1
-shift
+[ "$MANGO_INSTANCE_SIGNATURE" ] || {
+	echo no mango socket environment variable
+	exit 1
+}
+
 programName=${0##*/}
 stateFile=/tmp/$programName.state
-touch "$stateFile"
 
 get(){
 	term=$1
+	key=$2 # number
+	val=$3 # number
 	(printf '%s\0\n' "$term"; cat "$stateFile") | awk 'BEGIN{RS="\0\n"; FS="\0"; ORS=""}{
 		if (NR==1) term=$0; else {
-			if ($1 == term) {
-				print $2
+			if ($'"$key"' == term) {
+				print $'"$val"'
 				exit
 			}
 		}
@@ -46,6 +38,54 @@ remove(){
 		}
 	}' > "$tmp"
 	mv "$tmp" "$stateFile"
+}
+
+daemon(){
+	trap 'kill $!; continue' USR1
+	trap 'rm "$stateFile"' EXIT
+	trap exit INT TERM HUP
+	# basically store pid in statefile per command, signal daemon with usr1 to waitpid additional
+	# things. to keep process count low, use 1 waitpid command, have it exit if any pids exit, and on
+	# exit, inspect pid list to see what died. slight race condition risk but it will be fine. remove
+	# the dead pid from statefile. also store mango pid to see if that died, in which case just remove
+	# statefile.
+	waitpid 1 &
+	wait # wait for first signal :)
+	while true; do
+		pids=$(<"$stateFile" awk 'BEGIN{RS="\0\n"; FS="\0"; ORS=""}{ if (NR>=2) print $3 " "}')
+		[ "$(echo $pids)" ] || exit
+		waitpid -c 1 $mangopid $pids &
+		wait
+		[ -e /proc/$mangopid ] || exit
+		for pid in $pids; do
+			[ -e /proc/$pid ] || {
+				key=$(get $pid 3 1)
+				remove "$key"
+			}
+		done
+	done
+}
+
+scale=0.5
+while true; do
+	case $1 in
+		-daemon) daemon; exit;;
+		-wh) wh=$2; shift 2;;
+		-s) scale=$2; shift 2;;
+		-f) fullscreen=1; shift;;
+		-k) keep=1; shift;;
+		--) shift; break;;
+		*) break;;
+	esac
+done
+idKey=$1
+shift
+
+daemonPid=$(get daemonpid 1 3)
+[ "$daemonPid" ] || {
+	mangopid=$(echo "${MANGO_INSTANCE_SIGNATURE##*/}" | tr -cd 0-9)
+	"$0" -daemon & daemonPid=$!
+	printf '%s\0\0%s\0\n' daemonpid $daemonPid >"$stateFile"
 }
 
 present(){
@@ -88,12 +128,16 @@ getid(){
 		}' >"$fifo" &
 	read _ <"$fifo"
 	(sleep 10; echo timeout fail; kill 0) & pids=$!
-	"$@" &
+	"$@" & cmdpid=$!
 	(waitpid $!; echo command fail; kill 0) & pids="$pids $!"
 	read -r id <"$fifo"
 	kill $pids
 	rm "$fifo"
 	unset fifo
+
+	printf '%s\0%s\0%s\0\n' "$idKey" "$id" "$cmdpid" >>"$stateFile"
+	echo kill -s USR1 "$daemonPid"
+	kill -s USR1 "$daemonPid"
 }
 
 getinfo(){
@@ -109,11 +153,9 @@ begin(){
 	getid "$@"
 
 	present "$id"
-
-	printf '%s\0%s\0\n' "$idKey" "$id" >>"$stateFile"
 }
 
-id=$(get "$idKey"; echo x)
+id=$(get "$idKey" 1 2; echo x)
 id=${id%x}
 if [ "$id" ]; then
 	minimized=$(mmsg get client "$id" | jq -r '.is_minimized')
